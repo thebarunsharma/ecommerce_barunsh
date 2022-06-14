fd0306fbb2c2bdae7e2e3af600754d8ee102402d
Prerana Mali <prerana@Preranas-MacBook-Air.local>
2022-06-14 18:38:30 +0545

    New commit

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import guide, package, booking, agency
+from .models import guide, package, booking, agency, user
 
 admin.site.register(guide)
+admin.site.register(user)
 admin.site.register(package)
 admin.site.register(booking)
 admin.site.register(agency)
